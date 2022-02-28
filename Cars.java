09b0c0252e17d7160072d2532fea66f05e6bab32
FADEL DIOP <64317234+Fadel-Dev@users.noreply.github.com>
2022-02-28 11:31:54 +0000

    Add files via upload
    notion d heritage

---
+++ Cars.java
@@ -0,0 +1,9 @@
+package sn.fadel_dev;
+
+public class Cars extends Vehicule{
+
+
+    public Cars(String name, int speed) {
+        super(name, speed);
+    }
+}
---
+++ Moto.java
@@ -0,0 +1,10 @@
+package sn.fadel_dev;
+
+public class Moto extends  Vehicule {
+
+    public Moto(String name, int speed) {
+        super(name, speed);
+    }
+
+
+}
